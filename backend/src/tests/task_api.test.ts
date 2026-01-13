6617e9c9e9be46d017ba56ae1a54e05795f958ed
Se Joon Park <sparky9043@gmail.com>
2026-01-13 12:23:47 +0900

    add test file and fetching data tests

---
+++ backend/src/tests/task_api.test.ts
@@ -0,0 +1,24 @@
+import { test, after, describe } from 'node:test';
+import assert from 'node:assert';
+import pool from '../db/pool';
+import app from '../app';
+import supertest from 'supertest';
+
+const api = supertest(app);
+const baseUrl = '/api/tasks'
+
+void describe('GET Requests', () => {
+  void test('/api/tasks', async () => {
+    const response = await api
+    .get(baseUrl)
+    .expect(200);
+
+    console.log(response.body);
+    
+    assert.strictEqual(1, 1);
+  });
+});
+
+after(async () => {
+  await pool.end();
+});
