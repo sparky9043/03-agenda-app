005feaf0701586aacc21fe84ff37bb2752fd67b9
Se Joon Park <sparky9043@gmail.com>
2026-01-13 12:13:17 +0900

    make task router, service and repository files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import ping from './routers/ping.router';
+import task from './routers/task.router';
 
 const app = express();
 app.use(express.json());
 
 app.use('/ping', ping);
+app.use('/api/tasks', task);
 
 export default app;
---
+++ backend/src/routers/task.router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const task = Router();
+
+export default task;
