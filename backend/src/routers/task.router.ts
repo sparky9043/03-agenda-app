06b04020c872533f7de47af4d6df65e16773ec0e
Se Joon Park <sparky9043@gmail.com>
2026-01-13 17:14:04 +0900

    add try/catch statements to all router async functions

--- backend/src/routers/task.router.ts
+++ backend/src/routers/task.router.ts
@@ -1,22 +1,34 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import taskService from '../service/task';
 import { NewTaskEntry, Task } from "../types/types";
 
 const task = Router();
 
-task.get('/', async (_req, res: Response<Task[]>) => {
-  const savedTasks = await taskService.getTasks();
-  res.json(savedTasks);
+task.get('/', async (_req, res: Response<Task[]>, next: NextFunction) => {
+  try {
+    const savedTasks = await taskService.getTasks();
+    res.json(savedTasks);
+  } catch (error: unknown) {
+    next(error);
+  }
 });
 
-task.get('/:id', async (req: Request<{ id: string }>, res: Response<Task>) => {
-  const savedTask = await taskService.getTaskById(Number(req.params.id));
-  res.json(savedTask);
+task.get('/:id', async (req: Request<{ id: string }>, res: Response<Task>, next: NextFunction) => {
+  try {
+    const savedTask = await taskService.getTaskById(Number(req.params.id));
+    res.json(savedTask);
+  } catch (error: unknown) {
+    next(error);
+  }
 });
 
-task.post('/', async (req: Request<unknown, unknown, NewTaskEntry>, res: Response<Task>) => {
-  const savedTask = await taskService.addTask(req.body);
-  res.status(201).json(savedTask);
+task.post('/', async (req: Request<unknown, unknown, NewTaskEntry>, res: Response<Task>, next: NextFunction) => {
+  try {
+    const savedTask = await taskService.addTask(req.body);
+    res.status(201).json(savedTask);
+  } catch (error: unknown) {
+    next(error);
+  }
 });
 
 export default task;
